5521505f2b2d5046a890b254729d60597cc078db
Fabrício Gama <toka.fgg@gmail.com>
2022-04-19 20:00:13 -0300

    inicial migrations

--- RCstore/settings.py
+++ RCstore/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'users.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User Model
+
+AUTH_USER_MODEL = "users.User"
